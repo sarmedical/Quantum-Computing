7f84f8da01f47dc089cb25c476de4656cf250da1
sarmedical <50419572+sarmedical@users.noreply.github.com>
2019-08-13 22:12:02 -0400

    Add files via upload

--- Quantum_Teleportation.py
+++ Quantum_Teleportation.py
@@ -9,41 +9,55 @@ from projectq.backends import CircuitDrawer
 from projectq.meta import Dagger, Control
 from projectq import MainEngine
 
-def create_bell_pair(engine):
+def create_bell_pair(eng):
     """ returns a pair of entangled qubits"""
-    
-    q1 = engine.allocate_qubit()
-    q2 = engine.allocate_qubit()
-    
+    # q1 is our qubit / message 
+    q1 = eng.allocate_qubit()
+    # q2 is the receivers qubit, will be used to re-create the message state
+    q2 = eng.allocate_qubit()
+    # Hadamard gate to put q1 in superposition
     H | q1
+    # CNOT gate to flip q2 conditionally on the first qubit being in state |1>
     CNOT | (q1,q2)
     
     return q1,q2
 
-def run_teleport(eng):
-    # create bell pair
-    q1,q2 = create_bell_pair(eng) 
-    # Alice's qubit (to be teleported)
-    psi = eng.allocate_qubit()                                                 
-    # Entangle psi with q1 (her share of the bell-pair)
-    CNOT | (q1 , psi)
-    # measure two values (once in Hadamard basis) and send the bits to Bob
-    H | psi
-    Measure | psi
-    Measure | q1 
-    msg_to_Bob = [int(psi) , int(q1)]
+def create_message(eng,q1,message_value=0):
+    # qubit to send
+    qs = eng.allocate_qubit() 
+    if message_value == 1:
+        X | qs
+    # entangle the original qubit with the message qubit
+    CNOT | (qs,q1)
+    # 1_ put the message qubit in superposition
+    # 2_ measure the 2 values to get the classical bit value
+    H | qs
+    Measure | qs
+    Measure | q1
     
-    # Bob may have to apply up to two operation depending on the message sent
-    # by Alice:
-    with Control(eng,q1):
+    classical_message = [int(qs),int(q1)]
+    
+    return classical_message
+
+def message_receiver(eng,message,q2):
+    
+    if message[1] == 1:
         X | q2
-    with Control(eng,psi):
+    if message[0] == 1:
         Z | q2
-        
+    
     Measure | q2
+    
     eng.flush()
-    print ( int(q2) )
+    
+    received_bit = int(q2)
+    
+    return received_bit
     
 if __name__ == '__main__' :
     eng = MainEngine()
-    run_teleport(eng)
+    
+    q1 , q2 = create_bell_pair(eng)
+    message = create_message(eng,q1)
+    print( message_receiver(eng,message,q2) )
+    
